3e703ec991e486cb5caa2f3432fc0236850fed6d
crazyfrank <axu9417@gmail.com>
2025-06-02 21:10:44 +0800

    feat(10,15):79,72

--- 10-backtrack/79-word-find/main.go
+++ 10-backtrack/79-word-find/main.go
@@ -41,31 +41,6 @@ func exist(board [][]byte, word string) bool {
 	return false
 }
 
-func minDistance(word1 string, word2 string) int {
-	dp := make([][]int, len(word1)+1)
-	for i := 0; i <= len(word1); i++ {
-		dp[i] = make([]int, len(word2)+1)
-	}
-	for i := 0; i <= len(word1); i++ {
-		dp[i][0] = i
-	}
-	for j := 0; j <= len(word2); j++ {
-		dp[0][j] = j
-	}
-	for i := 1; i <= len(word1); i++ {
-		for j := 1; j <= len(word2); j++ {
-			if word1[i-1] == word2[j-1] {
-				dp[i][j] = dp[i-1][j-1]
-			} else {
-				dp[i][j] = min(min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+1)
-			}
-		}
-	}
-	fmt.Println(dp)
-	return dp[len(word1)][len(word2)]
-}
-
 func main() {
-	//fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"))
-	minDistance("horse", "ros")
+	fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"))
 }
